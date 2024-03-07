efd84af67a4539723f26acf7b44a894f44ea6c65
Ramazan <sakenovramazan@gmail.com>
2024-03-07 22:00:08 +0600

    main method changed

--- script.java
+++ script.java
@@ -3,5 +3,6 @@ import org.w3c.dom.ls.LSOutput;
 public class script {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Hi hello");
     }
 }
